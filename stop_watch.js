1c63a16f9cd0695f1957e8554111a208c4b1f32f
Niteshupadhyay085 <niteshupadhayay927@gmail.com>
2023-10-28 00:16:18 +0530

    Add file javaScript Html And css File

---
+++ stop_watch.js
@@ -0,0 +1,43 @@
+let [sec, min, hur] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+function stopwatch() {
+    sec++;
+    if(sec===60) {
+        sec=0;
+        min++;
+        if(min===60) {
+            min=0;
+            hur++;
+        }
+    }
+    let h = hur < 10 ? "0" + hur : hur;
+    let m = min < 10 ? "0" + min : min;
+    let s = sec < 10 ? "0" + sec : sec;
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+function watchStart() {
+    if(timer!==null) {
+        clearInterval(timer);
+    }
+   timer = setInterval(stopwatch,1000);
+}
+
+function watchStop() {
+    clearInterval(timer);
+}
+function watchReset() {
+    clearInterval(timer);
+    [sec, min, hur] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+}
+
+
+function watchReset() {
+    clearInterval(timer);
+    [sec, min, hur] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+
+}
